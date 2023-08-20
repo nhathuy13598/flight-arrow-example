714569443dea08a9b3d3dbd053478a19bc53f46a
nhathuy <nhathuy13598@gmail.com>
2023-08-20 14:42:01 +0700

    Init

---
+++ flight_client.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World from flight_client!" << std::endl;
+    return 0;
+}
---
+++ flight_server.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World from flight_server!" << std::endl;
+    return 0;
+}
